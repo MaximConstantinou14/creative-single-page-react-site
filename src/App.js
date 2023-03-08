5365453d1156d2ec027a3ed0d03617fddfe841fe
Maxim Constantinou <72621068+MaximConstantinou14@users.noreply.github.com>
2023-03-08 23:41:44 +0200

    Finished project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
 
 import Navbar from './Components/Navbar';
+import Header from './Components/Header';
+import Hero from './Components/Hero';
+import Carousel from './Components/Carousel';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Header />
+      <Hero />
+      <Carousel />
+      <Footer />
     </div>
   );
 }
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import React from 'react';
+import "../CSS/variables.css";
+import "../CSS/header.css";
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header className="header_wrapper">
+        <div className="header_img_section"></div>
+        <div className="header_text_section">
+            <h1 className="header_title">Branding & website design agency</h1>
+            <p className="header_description">
+              We specialize in visual storytelling by creating
+               cohesive brand and website design solutions for 
+               small businesses, giving lasting impressions to 
+               audiences in a digital world.
+            </p>
+            <button className="header_btn">learn more</button>
+        </div>
+    </header>
   )
 }
 
